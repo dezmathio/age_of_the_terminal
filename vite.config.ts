6511dc6bf0218242d148fb4a3e1abeecc15a5773
dezmathio <dezmathio@gmail.com>
2026-02-13 18:29:34 -0500

    match gh repo name

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Root in dev so you can use http://localhost:5173/ ; repo path only for production (GitHub Pages)
-  base: process.env.NODE_ENV === "production" ? "/age-of-the-terminal/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/age_of_the_terminal/" : "/",
 });
